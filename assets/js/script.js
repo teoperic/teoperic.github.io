3963065940c4fec1aa510738757abdefd4cffe14
teoperic <138066144+teoperic@users.noreply.github.com>
2025-07-29 23:00:50 +0200

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-/*more details button*/
-const container = document.querySelector(".info-box");
-const toggle = document.querySelector(".toggle");
-toggle.addEventListener("click", () => {
-  container.classList.toggle("active");
-});
-
-// on load name effect 
-setTimeout(() => document.querySelector('.name').classList.add('loaded'), 500);
